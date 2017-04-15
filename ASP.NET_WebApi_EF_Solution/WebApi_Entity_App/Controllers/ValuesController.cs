6efe050749ee5ef1e97a8fd8a02fc56730aca629
narekye <yegoryan.narek@gmail.com>
2017-04-14 23:08:26 -0600

    :v: Values controller update :v:

--- ASP.NET_WebApi_EF_Solution/WebApi_Entity_App/Controllers/ValuesController.cs
+++ ASP.NET_WebApi_EF_Solution/WebApi_Entity_App/Controllers/ValuesController.cs
@@ -8,37 +8,14 @@ using Json;
 
 namespace WebApi_Entity_App.Controllers
 {
-    
-    public class ValuesController : ApiController,IController
+
+    public class ValuesController : ApiController, IController
     {
         private PlayersContext db = new PlayersContext();
         public IEnumerable<string> Get()
         {
             return new[] { "value1", "value2" };
         }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
-
-        
         [System.Web.Http.HttpGet]
         public IHttpActionResult GetPlayers()
         {
@@ -48,7 +25,6 @@ namespace WebApi_Entity_App.Controllers
 
         public void Execute(RequestContext requestContext)
         {
-            
         }
     }
 }
