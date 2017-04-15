0ed9c09eb1e41aecb1f6bc43b1f9e57260427dde
narekye <yegoryan.narek@gmail.com>
2017-04-15 13:21:32 -0600

    :boom: some changes :boom:

--- ASP.NET_WebApi_EF_Solution/WebApi_Entity_App/Controllers/ValuesController.cs
+++ ASP.NET_WebApi_EF_Solution/WebApi_Entity_App/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Newtonsoft.Json;
 using WebApi_Entity_App.Database;
 namespace WebApi_Entity_App.Controllers
 {
@@ -10,6 +12,12 @@ namespace WebApi_Entity_App.Controllers
     public class ValuesController : ApiController, IController
     {
         private PlayersContext db = new PlayersContext();
+
+        public void Execute(RequestContext requestContext)
+        {
+
+        }
+
         public IEnumerable<string> Get()
         {
             return new[] { "value1", "value2" };
@@ -21,8 +29,25 @@ namespace WebApi_Entity_App.Controllers
             return Ok(players);
         }
 
-        public void Execute(RequestContext requestContext)
+        public IHttpActionResult Input()
         {
+            string[] result = new string[]
+            {
+                "Value1",
+                "value2"
+            };
+
+            string vb = "Hello";
+            return Json(new
+            {
+                foo = "bar",
+                boo = "bar"
+            });
+        }
+
+        public JsonResult YYYY()
+        {
+            return Json("aaaaaa", JsonRequestBehavior.AllowGet);
         }
     }
 }
