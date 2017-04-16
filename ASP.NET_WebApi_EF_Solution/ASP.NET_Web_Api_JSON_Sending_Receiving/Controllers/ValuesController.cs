d433c43f8fcb5a56599af27706f159cecd15956b
narekye <yegoryan.narek@gmail.com>
2017-04-15 19:31:25 -0600

    :v: added Person class :v:

--- ASP.NET_WebApi_EF_Solution/ASP.NET_Web_Api_JSON_Sending_Receiving/Controllers/ValuesController.cs
+++ ASP.NET_WebApi_EF_Solution/ASP.NET_Web_Api_JSON_Sending_Receiving/Controllers/ValuesController.cs
@@ -1,39 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
+using ASP.NET_Web_Api_JSON_Sending_Receiving.Models;
 
 namespace ASP.NET_Web_Api_JSON_Sending_Receiving.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        #region Comment
+        //// GET api/values
+        ////public IEnumerable<string> Get()
+        ////{
+        ////    return new string[] { "value1", "value2" };
+        ////}
+
+        //// GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+
+        //}
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+        #endregion Comment
+        // you must comment one post method... 
+
+        public IHttpActionResult Post([FromUri]Person p)
         {
-            return new string[] { "value1", "value2" };
+            if (ModelState.IsValid)
+                return this.Ok("OK");
+            return this.BadRequest("Please verify your data....!");
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        public IHttpActionResult PostPerson([FromBody] Person p)
         {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            if (ModelState.IsValid)
+                return Ok("From body ok");
+            return NotFound();
         }
     }
 }
---
+++ ASP.NET_WebApi_EF_Solution/ASP.NET_Web_Api_JSON_Sending_Receiving/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Web_Api_JSON_Sending_Receiving.Models
+{
+    public class Person
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
