ec136d8ecd289f98664020ff7eab2cf635f281ab
narekye <yegoryan.narek@gmail.com>
2017-04-19 18:25:46 -0600

    :v: GET fully completed :v:

--- Full_REST/Client/MainWindow.xaml.cs
+++ Full_REST/Client/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Web.Script.Serialization;
 using System.Windows.Controls;
@@ -22,8 +24,10 @@ namespace Client
     {
         private readonly HttpClient client = new HttpClient() { BaseAddress = new Uri("http://localhost:20989") };
         private HttpResponseMessage response = null;
+
         public MainWindow()
         {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             InitializeComponent();
         }
 
@@ -31,15 +35,10 @@ namespace Client
 
         private async void Get_All_Books(object sender, RoutedEventArgs e)
         {
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             try
             {
-                response = await client.GetAsync("/api/books");
-                response.EnsureSuccessStatusCode();
-                var result = response.Content.ReadAsStringAsync().Result;
-                var list = JsonConvert.DeserializeObject<List<Book>>(result);
-                foreach (Book book in list)
-                    res.Text += book.Author + "\t" + book.Name + "\t" + book.PublishDate + "\n";
+                var list = await Get_Deserialize("/api/books", true);
+                Print(list);
             }
             catch
             {
@@ -47,6 +46,37 @@ namespace Client
             }
         }
 
+        private async void Get_Book_By_Id(object sender, RoutedEventArgs e)
+        {
+            res.Text = " ";
+            if (idbox.Text == "") MessageBox.Show("Please set id", "About id...");
+            try
+            {
+                var list = await Get_Deserialize("/api/books/", false);
+                Print(list);
+                idbox.Clear();
+            }
+            catch { throw new InvalidOperationException(); }
+        }
 
+        private async Task<List<Book>> Get_Deserialize(string uri, bool flag)
+        {
+            response = await client.GetAsync(uri + idbox.Text);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            if (flag)
+            {
+                return JsonConvert.DeserializeObject<List<Book>>(result);
+            }
+            Book book = JsonConvert.DeserializeObject<Book>(result);
+            return new List<Book> { book };
+        }
+
+        private void Print(List<Book> list)
+        {
+            res.Text = "";
+            foreach (Book book in list)
+                res.Text += book.Author + "\t" + book.Name + "\t" + book.PublishDate + "\n";
+        }
     }
 }
