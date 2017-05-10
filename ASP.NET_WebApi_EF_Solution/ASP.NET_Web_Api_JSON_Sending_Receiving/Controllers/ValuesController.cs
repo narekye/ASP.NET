346da98a8f755462c6ce6fc6fe9b4fc4b7901e1e
narekye <yegoryan.narek@gmail.com>
2017-05-09 21:25:15 -0600

    :v:

--- ASP.NET_WebApi_EF_Solution/ASP.NET_Web_Api_JSON_Sending_Receiving/Controllers/ValuesController.cs
+++ ASP.NET_WebApi_EF_Solution/ASP.NET_Web_Api_JSON_Sending_Receiving/Controllers/ValuesController.cs
@@ -5,38 +5,6 @@
 
     public class ValuesController : ApiController
     {
-        #region Comment
-        //// GET api/values
-        ////public IEnumerable<string> Get()
-        ////{
-        ////    return new string[] { "value1", "value2" };
-        ////}
-
-        //// GET api/values/5
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/values
-        //public void Post([FromBody]string value)
-        //{
-
-        //}
-
-        //// PUT api/values/5
-        //public void Put(int id, [FromBody]string value)
-        //{
-
-        //}
-
-        //// DELETE api/values/5
-        //public void Delete(int id)
-        //{
-        //}
-        #endregion Comment
-        // you must comment one post method... 
-
         public IHttpActionResult Post([FromUri]Person p)
         {
             if (ModelState.IsValid)
