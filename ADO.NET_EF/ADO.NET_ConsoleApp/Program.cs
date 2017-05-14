87078a152182706c7931390579b634380f83fd89
narekye <yegoryan.narek@gmail.com>
2017-05-14 15:50:28 -0600

    :v: select completed :v:

--- ADO.NET_EF/ADO.NET_ConsoleApp/Program.cs
+++ ADO.NET_EF/ADO.NET_ConsoleApp/Program.cs
@@ -12,11 +12,27 @@ namespace ADO.NET_ConsoleApp
     {
         public static void Main()
         {
-           var path = ConfigurationManager.ConnectionStrings["entity"].ToString();
+            var path = ConfigurationManager.ConnectionStrings["entity"].ToString();
 
-            SqlConnection conn = new SqlConnection(path);
-            conn.Open();
-
+            using (SqlConnection conn = new SqlConnection(path))
+            {
+                try
+                {
+                    conn.OpenAsync().Wait();
+                    SqlCommand command = new SqlCommand("Select * from Books", conn);
+                    SqlDataReader reader = command.ExecuteReader();
+                    Console.WriteLine(reader.GetName(0) + "\t" + reader.GetName(1));
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetValue(0) + "\t" + reader.GetValue(1));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.Read();
         }
     }
 }
