1ed41c9466539dc97e5f6247d0ca5c15a71e9528
narekye <yegoryan.narek@gmail.com>
2017-05-15 14:20:19 -0600

    :v: new project adding :v:

---
+++ Database_Project/ConsoleAppDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConsoleAppDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = "Data Source=.;initial catalog=newusers;integrated security=True";
+
+            using (SqlConnection conn = new SqlConnection(path))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();
+                SqlCommand command = conn.CreateCommand();
+
+                command.Transaction = transaction;
+                command.CommandText = "Select * from Users";
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    Console.WriteLine(reader.GetInt32(0) + "  " + reader.GetString(1) + "  " + reader.GetString(2));
+                
+                // command.CommandText = $"INSERT INTO Users(FirstName,LastName) VALUES ({Console.ReadLine()},{Console.ReadLine()})";
+                // command.ExecuteNonQuery();
+                // Console.WriteLine(count);
+                Console.WriteLine("Close");
+            }
+        }
+    }
+}
---
+++ Database_Project/Users/Tables/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT PRIMARY KEY identity(1,1),
+	[FirstName] varchar(50) not null,
+	[LastName] varchar(50) not null
+)
