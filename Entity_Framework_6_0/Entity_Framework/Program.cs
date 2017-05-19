c75fe52c43136685344ab1086f8deb4766f327c5
narekye <yegoryan.narek@gmail.com>
2017-05-18 18:31:37 -0600

    :v: edmx generated :v:

---
+++ Entity_Framework_6_0/Entity_Framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
